cfcf99e648fdb57d1737e1ebf5ffb260de311cb8
Foissac-Nicola <12102598+Foissac-Nicola@users.noreply.github.com>
2018-11-21 13:31:40 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='named_entity_scraper',
+    version='v0.1',
+    packages=[''],
+    url='',
+    license='MIT',
+    author='Loic Favreliere , Nicola Foissac',
+    author_email='loic.favreliere@etudiant.univ-lr.fr , nicola.foissac@etudiant.univ-lr.fr',
+    description='',
+    install_requires=
+    [
+        'jusText==2.2.0',
+        'nltk==3.4',
+        'spacy==2.0.16'
+
+    ]
+
+)
