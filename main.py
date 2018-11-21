2ce3307ebeabe46dd4fc8174cccff5b231c669c9
Loic Favreliere <loic.favreliere@etudiant.univ-lr.fr>
2018-11-21 13:13:48 +0100

    First implementation of NER

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from collections import Counter
 
 from pprint import pprint
 
+
 def preprocess(sent):
     sent = nltk.word_tokenize(sent)
     sent = nltk.pos_tag(sent)
@@ -35,10 +36,20 @@ def main():
     nlp = spacy.load('en_core_web_sm')
 
     doc = nlp('European authorities fined Google a record $5.1 billion on Wednesday for abusing its power in the mobile phone market and ordered the company to alter its practices')
-    # pprint([(X.text, X.label_) for X in doc.ents])
+    pprint([(X.text, X.label_) for X in doc.ents])
 
-    pprint([(X, X.ent_iob_, X.ent_type_) for X in doc])
-    
+    # pprint([(X, X.ent_iob_, X.ent_type_) for X in doc])
+
+    labels = [x.label_ for x in doc.ents]
+    items = [x.text for x in doc.ents]
+
+    pprint(labels)
+    pprint(items)
+
+
+    # Counter(items).most_common(3)
+    # pprint(Counter(labels))
+
 
 
 if __name__ == '__main__':
